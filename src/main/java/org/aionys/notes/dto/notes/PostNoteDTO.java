019af487c1335af845e55be98fa141e05271277a
Evemose <rotan.olexandr@lll.kpi.ua>
2024-05-30 19:24:40 +0300

    update post dto

--- src/main/java/org/aionys/notes/dto/notes/PostNoteDTO.java
+++ src/main/java/org/aionys/notes/dto/notes/PostNoteDTO.java
@@ -1,9 +1,12 @@
 package org.aionys.notes.dto.notes;
 
 import jakarta.validation.constraints.NotBlank;
+import org.aionys.notes.valiation.annotations.NullOrNotBlank;
+import org.aionys.notes.valiation.groups.Full;
+import org.aionys.notes.valiation.groups.Partial;
 
 public record PostNoteDTO(
-        @NotBlank String title,
-        @NotBlank String content
+        @NotBlank(groups = Full.class) @NullOrNotBlank(groups = Partial.class) String title,
+        @NotBlank(groups = Full.class) @NullOrNotBlank(groups = Partial.class) String content
 ) {
 }
