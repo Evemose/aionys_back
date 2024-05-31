fc755f4fc9ec0a4d890977bcee9c5b74a8dcb25d
Evemose <rotan.olexandr@lll.kpi.ua>
2024-05-31 22:27:10 +0300

    added guard clause to register endpoint

--- src/main/java/org/aionys/main/security/users/UserController.java
+++ src/main/java/org/aionys/main/security/users/UserController.java
@@ -3,6 +3,7 @@ package org.aionys.main.security.users;
 import lombok.RequiredArgsConstructor;
 import org.aionys.main.commons.valiation.groups.Full;
 import org.aionys.main.security.jwt.JwtEncryptor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +28,15 @@ class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<GetUserDTO> register(@RequestBody @Validated(Full.class) PostUserDTO dto) {
+    public ResponseEntity<GetUserDTO> register(
+            @RequestBody @Validated(Full.class) PostUserDTO dto,
+            Principal principal) {
+        // for some, reason, not().authenticated()
+        // does not permit access for non-authenticated users
+        // so we have to check it manually
+        if (principal != null) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
         return ResponseEntity.ok(userMapper.toDTO(userService.save(userMapper.toEntity(dto))));
     }
 }
