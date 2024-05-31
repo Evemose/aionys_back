1e110d0be03cb88bd8824646b747ca29f67442b5
Evemose <rotan.olexandr@lll.kpi.ua>
2024-05-31 22:26:54 +0300

    disabled session management

--- src/main/java/org/aionys/main/security/SecurityConfig.java
+++ src/main/java/org/aionys/main/security/SecurityConfig.java
@@ -4,13 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.aionys.main.security.jwt.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -23,6 +20,9 @@ class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable) // TODO: configure CORS properly
                 .authorizeHttpRequests(authorizeRequests ->
