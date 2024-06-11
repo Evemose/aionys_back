1f0d2508810efd57f17d0480a6dc168ac894ab9e
Evemose <rotan.olexandr@lll.kpi.ua>
2024-06-11 23:27:35 +0300

    configured mapper to send dates in iso 8601 format

--- src/main/java/org/aionys/main/commons/ObjectMapperConfig.java
+++ src/main/java/org/aionys/main/commons/ObjectMapperConfig.java
@@ -1,14 +1,20 @@
 package org.aionys.main.commons;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 
 @Configuration
 public class ObjectMapperConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
-        return new ObjectMapper().findAndRegisterModules();
+        return new ObjectMapper()
+                .findAndRegisterModules()
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS) // write dates as ISO-8601 strings
+                .setDateFormat(new StdDateFormat());
     }
 }
